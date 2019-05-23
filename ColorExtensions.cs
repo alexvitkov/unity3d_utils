9f2b99b9a6a3f0879758aa796117be1f829fde37
alexvitkov <44268717+alexvitkov@users.noreply.github.com>
2019-05-23 12:41:34 +0300

    garb

---
+++ ColorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class ColorExtensions {
+
+	public static Color WithAlpha(this Color c, float a) 
+		=> new Color(c.r, c.g, c.b, a);
+
+	public static Color R(this Color c, float r)
+		=> new Color(r, c.g, c.b, c.a);
+
+	public static Color G(this Color c, float g)
+		=> new Color(c.r, g, c.b, c.a);
+
+	public static Color B(this Color c, float b)
+		=> new Color(c.r, c.g, b, c.a);
+
+}
---
+++ TransformExtensions2D.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public static class TransformExtensions2D {
+
+	public static void SetScale(this Transform t, float scale) {
+		t.localScale = new Vector3(scale, scale, 1f);
+	}
+
+	public static float GetScale(this Transform t)
+		=> t.localScale.x;
+
+	public static void SetRotation(this Transform t, float angleDeg) {
+		t.localEulerAngles = new Vector3(0f, 0f, angleDeg);
+	}
+
+	public static float GetRotation(this Transform t)
+		=> t.eulerAngles.z;
+
+	public static void LookAt2D(this Transform t, Vector3 point) {
+		Vector3 dir = point - t.position;
+		float angleRad = Mathf.Atan2(dir.y, dir.x);
+		t.SetRotation(angleRad * Mathf.Rad2Deg);
+	}
+
+}

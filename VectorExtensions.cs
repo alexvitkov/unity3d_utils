2dd508945231ffe2509297602f8d598e39666b77
alexvitkov <44268717+alexvitkov@users.noreply.github.com>
2019-10-25 22:55:23 -0700

    added some vector stuff

--- VectorExtensions.cs
+++ VectorExtensions.cs
@@ -11,10 +11,21 @@ public static class VectorExtensions {
     public static Vector3 WithZ(this Vector3 v, float z)
         => new Vector3(v.x, v.y, z);
 
+    public static Vector2 WithMagnitude(this Vector3 vec, float mag)
+        => vec.normalized * mag;
+
+
+
     public static Vector2 WithX(this Vector2 v, float x)
         => new Vector2(x, v.y);
 
     public static Vector2 WithY(this Vector2 v, float y)
         => new Vector2(v.x, y);
+
+    public static Vector2 WithMagnitude(this Vector2 vec, float mag)
+        => vec.normalized * mag;
+        
+    public static float ToAngle(this Vector2 dir) 
+        => Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
 
 }

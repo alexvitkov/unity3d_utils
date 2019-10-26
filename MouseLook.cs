0b269441ca04d6bfc4912c74ed605556e2fd3f56
alexvitkov <44268717+alexvitkov@users.noreply.github.com>
2019-10-25 22:50:53 -0700

    cleaned up mouselook a bit

--- MouseLook.cs
+++ MouseLook.cs
@@ -5,10 +5,8 @@ public class MouseLook : MonoBehaviour {
     public Vector2 sensitivity = new Vector3(50, 65);
     public float smooth = 5;
 
-    public Transform body = null;
-    public Transform head = null;
-
-    private Vector2 angles, currentAngles;
+    public Transform body, head;
+    private Vector2 angles;
     
     void Start() {
         if (body == null) body = transform;
@@ -20,15 +18,17 @@ public class MouseLook : MonoBehaviour {
         angles.y -= Input.GetAxis("Mouse Y") * sensitivity.y * Time.deltaTime;
         angles.y = Mathf.Clamp(angles.y, -90f, 90f);
 
+        Vector3 ang = body.localEulerAngles;
+
         if (smooth > 0) {
-            currentAngles.x = Mathf.LerpAngle(currentAngles.x, angles.x, smooth * Time.deltaTime);
-            currentAngles.y = Mathf.LerpAngle(currentAngles.y, angles.y, smooth * Time.deltaTime);
+            ang.x = Mathf.LerpAngle(ang.x, angles.x, smooth * Time.deltaTime);
+            ang.y = Mathf.LerpAngle(ang.y, angles.y, smooth * Time.deltaTime);
         }
         else {
-            currentAngles = angles;
+            ang = angles;
         }
 
-        body.localEulerAngles = new Vector3(0f, currentAngles.x, 0f);
-        head.localEulerAngles = new Vector3(currentAngles.y, 0f, 0f);
+        body.localEulerAngles = new Vector3(0f, ang.x, 0f);
+        head.localEulerAngles = new Vector3(ang.y, 0f, 0f);
     }
 }

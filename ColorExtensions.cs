21a45e7af09981ab876633fbb499ea8c405eab5d
alexvitkov <44268717+alexvitkov@users.noreply.github.com>
2019-10-25 22:55:59 -0700

    renamed some colorextension functions

--- ColorExtensions.cs
+++ ColorExtensions.cs
@@ -5,13 +5,13 @@ public static class ColorExtensions {
 	public static Color WithAlpha(this Color c, float a) 
 		=> new Color(c.r, c.g, c.b, a);
 
-	public static Color R(this Color c, float r)
+	public static Color WithR(this Color c, float r)
 		=> new Color(r, c.g, c.b, c.a);
 
-	public static Color G(this Color c, float g)
+	public static Color WithG(this Color c, float g)
 		=> new Color(c.r, g, c.b, c.a);
 
-	public static Color B(this Color c, float b)
+	public static Color WithB(this Color c, float b)
 		=> new Color(c.r, c.g, b, c.a);
 
 }
